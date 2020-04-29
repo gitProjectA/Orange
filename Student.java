c72822f17c5e73ae72b1765c0f32bc17e0739705
gitProjectA <3425271303@qq.com>
2020-04-29 18:38:11 +0800

    添加Student

---
+++ Student.java
@@ -0,0 +1,17 @@
+ public class Student{
+
+      private String name;
+      private Integer age;
+      public void setName(String name){
+         this.name=name;
+      }
+      public String  getName(String  name){
+          return name;
+      }
+      public void setAge(Integer age){
+           this.age=age;
+      }
+      public Integer getAge(Integer age){
+           return age;
+      }
+ }
