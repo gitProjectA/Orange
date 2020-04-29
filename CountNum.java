c43878050a7ce755334d88ea2574fa97eeccb27d
gitProjectB <2081262367@qq.com>
2020-04-29 10:31:07 +0800

    添加减法方法

--- CountNum.java
+++ CountNum.java
@@ -2,5 +2,8 @@ public  class CountNum{
      public double  add(Integer  a,Integer b){
 	     return  a+b;
      }
+     public double minus(Integer  a,Integer b){
 
+            return a-b;
+     }
 }
